a71652d5148dabf6dbb404d4551ea2e6cb241c91
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-01-19 22:56:26 +0100

    update paralax effect

--- js/home.js
+++ js/home.js
@@ -1,23 +1,41 @@
 
 var cover = function() {
-	var st = $(window).scrollTop()
-	var wh = $(window).innerHeight()
-	var eh = $(this).innerHeight()
-	var of = $(this).offset()
-	var diff
-	
-	if (st + wh > of.top) {
-		diff = (st + wh) - of.top
-		diff = diff > eh ? eh : diff
+    var st = $(window).scrollTop()
+    var bg = $(this).find('.background.nofixed')
+    var bgh = bg.innerHeight()
+    var bgf = $(this).find('.background.fixed')
+    var cbgf = $(this).find('.cover_bg')
+    var of = $(this).offset()
+    var diff = st - of.top
 
-		$(this).find('.cover').css('margin-top', '-' + diff + 'px')
-	}
+    if (st >= of.top && bgf.css('display') === 'none') {
+        bgf.css('display', 'block')
+        $(this).find('.cover').css('height', bgh + 'px')
+    } else if (st >= of.top && diff <= bgh) {
+        bgf.css('top', 0)
+        cbgf.css('height', diff + 'px')
+        cbgf.css('top', bgh - diff + 'px')
+        if (cbgf.css('display') === 'none') {
+            cbgf.css('display', 'block')
+        }
+    } 
+
+    if (diff >= bgh * 2) {
+        bgf.css('top', (bgh * 2) + 'px')
+    } else if (diff >= bgh) {
+        bgf.css('top', (bgh - diff) + 'px')
+        cbgf.css('top', bgh - diff + 'px')
+        cbgf.css('height', bgh + 'px')
+    } else if (diff <= 0) {
+        bgf.css('display', 'none')
+        cbgf.css('display', 'none')
+    }
 }
 
 
 $(document).ready(function() {
     $('.covered').each(function() {
-    	$(window).scroll(cover.bind(this))
-    	$(window).resize(cover.bind(this))
+        $(window).scroll(cover.bind(this))
+        $(window).resize(cover.bind(this))
     })
 })
