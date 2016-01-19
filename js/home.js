0d694d6d5f614825e37c514504c5d9cecb5a1800
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-01-19 23:05:06 +0100

    fix no scroll

--- js/home.js
+++ js/home.js
@@ -37,5 +37,6 @@ $(document).ready(function() {
     $('.covered').each(function() {
         $(window).scroll(cover.bind(this))
         $(window).resize(cover.bind(this))
+        cover.bind(this)()
     })
 })
