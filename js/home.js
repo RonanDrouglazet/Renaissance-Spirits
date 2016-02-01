119e91dd73a4271e44771f8fa7962a65c94cc12a
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-02-01 23:19:51 +0100

    slide push

--- js/home.js
+++ js/home.js
@@ -8,66 +8,74 @@ var cover = function() {
     var of = $(this).offset()
     var diff = st - of.top
 
-    if (st >= of.top && bgf.css('display') === 'none') {
+    if (st >= of.top && bgf.css('visibility') === 'hidden') {
         // when we begin to scroll in parallax
-        bgf.show()
+        bgf.css('visibility', 'visible')
         $(this).find('.cover').css('height', bgh + 'px')
     } else if (st >= of.top && diff <= bgh) {
         // then
         bgf.css('top', 0)
         cbgf.css('height', diff + 'px')
         cbgf.css('top', bgh - diff + 'px')
-        if (cbgf.css('display') === 'none') {
-            cbgf.show()
+        if (cbgf.css('visibility') === 'hidden') {
+            cbgf.css('visibility', 'visible')
         }
     }
 
     if (diff >= bgh * 2) {
         // when we scroll out parallax completely
         bgf.css('top', (bgh * 2.5) + 'px')
-        bgf.hide()
-        cbgf.hide()
+        bgf.css('visibility', 'hidden')
+        cbgf.css('visibility', 'hidden')
     } else if (diff >= bgh) {
         // when we begin to leave parallax
         bgf.css('top', (bgh - diff) + 'px')
         cbgf.css('top', bgh - diff + 'px')
         cbgf.css('height', bgh + 'px')
-        if (cbgf.css('display') === 'none') {
-            cbgf.show()
+        if (cbgf.css('visibility') === 'hidden') {
+            cbgf.css('visibility', 'visible')
         }
     } else if (diff <= 0) {
         // when we are just over original image
-        bgf.hide()
-        cbgf.hide()
+        bgf.css('visibility', 'hidden')
+        cbgf.css('visibility', 'hidden')
     }
 }
 
 var slide = function() {
-    var imgs = $(this).find('img.slide')
+    var _this = $(this)
+    var imgs = _this.find('img.slide')
     var current = 0
-    var start = '100%'
+    var start = 100
     var end = '0%'
     var rtl = true
 
+    
+
+    _this.css('transition', 'left 2s ease-in-out')
+    _this.css('left', '0%')
+
+    if (_this.hasClass('fixed') || _this.hasClass('cover_bg')) {
+        _this.css('width', (imgs.length * 100) + '%')
+        imgs.css('width', (100 / imgs.length) + '%')
+        start = 100 / imgs.length
+    }
+
     imgs.each(function(id, elm) {
-        if (id !== 0) {
-            $(elm).css('left', start)
-            setTimeout(function() { $(elm).css('transition', 'left 2s ease-in-out') }, 500)
-        }
+        $(elm).css('left', (start * id) + '%')
     })
 
     setInterval(function() {
-        if (current === imgs.length) {
+        if (current === imgs.length - 1) {
             rtl = false
         } else if (current === 0) {
             rtl = true
         }
 
         current = rtl ? current + 1 : current - 1
-
-        if (current !== 0) {
-            $(imgs[current]).css('left', rtl ? end : start)
-        }
+        console.log('current', current)
+        console.log('goto', (-100 *  current) + '%')
+        _this.css('left', (-100 *  current) + '%')
     }, 5000)
 }
 
