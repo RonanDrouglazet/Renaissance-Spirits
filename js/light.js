1a3ff148a254eaf8d3802ab69e9b0b1e1ca9e6a4
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-05-22 16:34:10 +0200

    fix #25

--- js/light.js
+++ js/light.js
@@ -1,32 +1,8 @@
 $(document).ready(function() {
-    var win = $(window);
-    var menu = $('header .menu');
-    var mheight =  menu.height();
-    var fmenu = $('header .floating');
 
     /************** 
      * MENU
      **************/
-    $('header .floating').click(function() {
-        if (fmenu.find('i').hasClass('sidebar')) {
-            menu.slideDown(500)
-            fmenu.find('i').removeClass('sidebar').addClass('sort ascending')
-        } else {
-            menu.slideUp(500)
-            fmenu.find('i').removeClass('sort ascending').addClass('sidebar')
-        }
-    })
-
-    win.on('scroll', function() {
-        if (win.scrollTop() > mheight && fmenu.css('display') !== 'block') {
-            menu.slideUp(500)
-            fmenu.find('i').removeClass('sort ascending sidebar').addClass('sidebar')
-            fmenu.fadeIn()
-        } else if (win.scrollTop() < mheight && fmenu.css('display') === 'block') {
-            menu.slideDown(500)
-            fmenu.fadeOut()
-        }
-    })
 
     $('header .selectbar').each(function(i, bar) {
         var select = $(this)
